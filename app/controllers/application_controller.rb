b9040dbdad3c55329320b02eb44c0b57a6efaef6
Frederic Bouquet <fbouquet@objetdirect.com>
2015-11-01 05:28:40 +0100

    Finish log in/log out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  def hello
-    render text: "hello, world!"
+  include SessionsHelper
+
+  def log_in(user)
+    session[:user_id] = user.id
   end
 end
